0620b96bbb78a162d6c5f5b3a0133d7d53e5132b
Gh1zu <100525788+Gh1zu@users.noreply.github.com>
2026-05-23 11:42:24 +0300

    removed sql db for a pandas data frame

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import streamlit as st
 import pandas as pd
-import sqlite3
 
-# define connection and cursor
+#create data frame
+if "df" not in st.session_state:
+    st.session_state.df=pd.DataFrame(columns=["ID","Name", "Email", "Phone number"])
 
-connection = sqlite3.connect("user_db.db") 
-
-cursor = connection.cursor()
-
-#create to_do table
-
-cursor.execute(""" CREATE TABLE IF NOT EXISTS
-users(user_id INTEGER PRIMARY KEY, user_name TEXT, user_email TEXT, user_phone_number TEXT)""")
-connection.commit()
-
+if "index" not in st.session_state:
+    st.session_state.index=0
 st.title("Add user:")
-
 # form
 
 with st.form("user_form", clear_on_submit=False):
@@ -25,13 +17,12 @@ with st.form("user_form", clear_on_submit=False):
     st.subheader("Email:")
     user_email=st.text_input(label="", label_visibility="collapsed", key="user_email", placeholder="example@email.com")
     st.subheader("Phone number:")
-    user_phone_number=st.text_input(label="", label_visibility="collapsed", key="user_phone_number", placeholder="###-###-####")
+    user_phone_number=st.text_input(label="", label_visibility="collapsed", key="user_phone_number", placeholder="###-###-###")
 
     submit=st.form_submit_button("Add")
 
     if submit:
         valid=1
-        
 
         if user_name == "" or user_email == "" or user_phone_number == "":
             st.badge("Please fill every row", icon=":material/error:", color="orange")
@@ -42,21 +33,28 @@ with st.form("user_form", clear_on_submit=False):
             except:
                 st.badge("Enter a valid phone number", icon=":material/error:", color="orange")
                 valid=0
-            if "@" not in user_email or "." not in user_email:
+            if len(user_phone_number)<9:
+                st.badge("Enter a valid phone number", icon=":material/error:", color="orange")
+                valid=0
+            if "@" not in user_email or "." not in user_email or len(user_email)<5:
                 st.badge("Enter a valid email", icon=":material/error:", color="orange")
                 valid=0
         
         if valid:
-            cursor.execute("INSERT INTO users(user_name, user_email, user_phone_number) VALUES(?, ?, ?)", (user_name, user_email, user_phone_number))
-            connection.commit()
-
-
+            st.session_state.index+=1
+            form_data = {
+                "ID": st.session_state.index,
+                "Name": user_name,
+                "Email": user_email,
+                "Phone number": user_phone_number
+            }
+            st.session_state.df.loc[len(st.session_state.df)]=form_data
+            
 # delete all
 if st.button("Delete all"):
-    cursor.execute("DELETE FROM users")
-    connection.commit()
+    st.session_state.df=st.session_state.df.iloc[0:0]
 
 # view
-df= pd.read_sql_query("SELECT * FROM users", connection)
 
-edited_df=st.data_editor(df, hide_index=True, num_rows="delete")
+edited_df=st.data_editor(st.session_state.df, hide_index=True, num_rows="delete", key="editor")
+st.session_state.df=edited_df
